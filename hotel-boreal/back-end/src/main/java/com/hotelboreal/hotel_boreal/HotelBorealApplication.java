32f2b638b54ec4cd3f863fd21edc12379b6ef858
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-08-01 14:33:06 -0300

    Iniciando projeto

---
+++ hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/HotelBorealApplication.java
@@ -0,0 +1,13 @@
+package com.hotelboreal.hotel_boreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBorealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBorealApplication.class, args);
+	}
+
+}
---
+++ hotel-boreal/back-end/src/test/java/com/hotelboreal/hotel_boreal/HotelBorealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelboreal.hotel_boreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBorealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
