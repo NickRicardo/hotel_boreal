699c58c46aa6ad5bcf891cb5327c432b4768223f
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-08-14 14:10:23 -0300

    implementado classe pessoa

--- hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/model/dto/QuartoDTO.java
+++ hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/model/dto/QuartoDTO.java
@@ -6,5 +6,29 @@ public class QuartoDTO {
 
     private int numero_do_quarto;
 
-    private
+    private int capacidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero_do_quarto() {
+        return numero_do_quarto;
+    }
+
+    public void setNumero_do_quarto(int numero_do_quarto) {
+        this.numero_do_quarto = numero_do_quarto;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
 }
--- hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/model/dto/ReservaDTO.java
+++ hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/model/dto/ReservaDTO.java
@@ -1,4 +1,26 @@
 package com.hotelboreal.hotel_boreal.model.dto;
 
+import java.time.LocalDate;
+
 public class ReservaDTO {
+
+    private LocalDate data_check_in;
+
+    private LocalDate data_check_out;
+
+    public LocalDate getData_check_in() {
+        return data_check_in;
+    }
+
+    public void setData_check_in(LocalDate data_check_in) {
+        this.data_check_in = data_check_in;
+    }
+
+    public LocalDate getData_check_out() {
+        return data_check_out;
+    }
+
+    public void setData_check_out(LocalDate data_check_out) {
+        this.data_check_out = data_check_out;
+    }
 }
