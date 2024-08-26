efe7fc40a8915cb4ca596edaf060d9dbb93bd6ca
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-08-26 20:38:30 -0300

    Fazendo Controller

--- hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/controller/ClienteController.java
+++ hotel-boreal/back-end/src/main/java/com/hotelboreal/hotel_boreal/controller/ClienteController.java
@@ -1,8 +1,45 @@
 package com.hotelboreal.hotel_boreal.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hotelboreal.hotel_boreal.model.Cliente;
+import com.hotelboreal.hotel_boreal.repository.ClienteRepository;
+import com.hotelboreal.hotel_boreal.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.ref.Cleaner;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+
+    @GetMapping
+    public List<Cliente> todosClientes() {
+        return clienteService.obterCliente();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Cliente> obterClientePorId(@PathVariable int id){
+        return clienteService.obterClientePorId(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletarCliente(@PathVariable int id){
+        clienteService.deletarCliente(id);
+    }
+
+    @PutMapping("{id}")
+    public Cliente atualizarCliente(@PathVariable int id, @RequestBody Cliente cliente){
+        return clienteService.atualizarCliente(id, cliente);
+    }
+
 }
 
